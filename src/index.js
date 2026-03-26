be01c74b0baf7b738573917dfc71579b7e4cb867
caractere-advertising <anne@caractere-advertising.be>
2026-03-26 13:21:24 +0100

    finetuning 26-03

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ import "./js/swiper-gallery.js";
 import "./js/header.js";
 import "./js/scroll-top.js";
 
+<<<<<<< HEAD
 import "./js/parallax.js";
+=======
+>>>>>>> 438a9d9 (finetuning 26-03)
 import "./js/img-hover.js";
 
 import "./js/parallax.js";
