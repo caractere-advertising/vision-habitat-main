775baf2c536504e823f313cc95d6aa4c28feecb0
caractere-advertising <anne@caractere-advertising.be>
2026-03-26 13:21:24 +0100

    finetuning 26-03

--- assets/dist/style.bundle.js
+++ assets/dist/style.bundle.js
@@ -1,5 +1,4 @@
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./node_modules/css-loader/dist/cjs.js??ruleSet[1].rules[0].use[1]!./node_modules/postcss-loader/dist/cjs.js??ruleSet[1].rules[0].use[2]!./src/tailwind.css"
@@ -8,6 +7,7 @@
   \******************************************************************************************************************************************************************/
 (module, __webpack_exports__, __webpack_require__) {
 
+"use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
@@ -2921,6 +2921,7 @@ ___CSS_LOADER_EXPORT___.push([module.id, `/*! tailwindcss v4.2.1 | MIT License |
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 () {
 =======
 =======
@@ -2942,6 +2943,8 @@ ___CSS_LOADER_EXPORT___.push([module.id, `/*! tailwindcss v4.2.1 | MIT License |
 >>>>>>> f8c007c (Optimisation section.php + css + ajout nouvelle section page "construire")
 =======
 >>>>>>> 02d2601 (fin 30-3)
+=======
+>>>>>>> 2968b22 (finetuning 26-03)
 =======
 >>>>>>> 048dcbb (npm run build)
 =======
@@ -6420,6 +6423,8 @@ body {
 =======
 =======
 >>>>>>> db64302 (fin 30-3)
+=======
+>>>>>>> 4b9d2f4 (finetuning 26-03)
 (module, __webpack_exports__, __webpack_require__) {
 
 __webpack_require__.r(__webpack_exports__);
@@ -8891,11 +8896,18 @@ body {
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 <<<<<<< HEAD
+<<<<<<< HEAD
 >>>>>>> bf87089 (Optimisation section.php + css + ajout nouvelle section page "construire")
 >>>>>>> 83ad8be (Optimisation section.php + css + ajout nouvelle section page "construire")
 =======
 >>>>>>> db64302 (fin 30-3)
+=======
+=======
+() {
+>>>>>>> 3d2ceae (finetuning 26-03)
+>>>>>>> 4b9d2f4 (finetuning 26-03)
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -8922,6 +8934,9 @@ throw new Error("Module build failed (from ./node_modules/sass-loader/dist/cjs.j
 >>>>>>> 781f678 (calc(100-60px))
 =======
 >>>>>>> 02d2601 (fin 30-3)
+=======
+throw new Error("Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nexpected \"{\".\n\u001b[34m   ╷\u001b[0m\n\u001b[34m25 │\u001b[0m <<<<<<< HEAD\u001b[31m\u001b[0m\n\u001b[34m   │\u001b[0m \u001b[31m            ^\u001b[0m\n\u001b[34m   ╵\u001b[0m\n  src/scss/variables/base.scss 25:13  @use\n  src/scss/main.scss 1:1              root stylesheet");
+>>>>>>> 2968b22 (finetuning 26-03)
 
 /***/ },
 
@@ -8931,6 +8946,7 @@ throw new Error("Module build failed (from ./node_modules/sass-loader/dist/cjs.j
   \*****************************************************/
 (module) {
 
+"use strict";
 
 
 /*
@@ -9025,6 +9041,7 @@ module.exports = function (cssWithMappingToString) {
   \************************************************************/
 (module) {
 
+"use strict";
 
 
 module.exports = function (item) {
@@ -9050,6 +9067,7 @@ module.exports = function (item) {
   \**************************/
 (__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 
+"use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
@@ -9102,6 +9120,7 @@ var update = _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js
   \****************************/
 (__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 
+"use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
@@ -9154,6 +9173,7 @@ var update = _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js
   \****************************************************************************/
 (module) {
 
+"use strict";
 
 
 var stylesInDOM = [];
@@ -9247,6 +9267,7 @@ module.exports = function (list, options) {
   \********************************************************************/
 (module) {
 
+"use strict";
 
 
 var memo = {};
@@ -9290,6 +9311,7 @@ module.exports = insertBySelector;
   \**********************************************************************/
 (module) {
 
+"use strict";
 
 
 /* istanbul ignore next  */
@@ -9309,6 +9331,7 @@ module.exports = insertStyleElement;
   \**********************************************************************************/
 (module, __unused_webpack_exports, __webpack_require__) {
 
+"use strict";
 
 
 /* istanbul ignore next  */
@@ -9328,6 +9351,7 @@ module.exports = setAttributesWithoutAttributes;
   \***************************************************************/
 (module) {
 
+"use strict";
 
 
 /* istanbul ignore next  */
@@ -9398,6 +9422,7 @@ module.exports = domAPI;
   \*********************************************************************/
 (module) {
 
+"use strict";
 
 
 /* istanbul ignore next  */
@@ -9495,8 +9520,9 @@ module.exports = styleTagTransform;
 /******/ 	
 /************************************************************************/
 var __webpack_exports__ = {};
-// This entry needs to be wrapped in an IIFE because it needs to be isolated against other modules in the chunk.
+// This entry needs to be wrapped in an IIFE because it needs to be in strict mode.
 (() => {
+"use strict";
 /*!**********************!*\
   !*** ./src/style.js ***!
   \**********************/
