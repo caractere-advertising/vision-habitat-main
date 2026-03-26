e3c847dc61f30ebfe6ade6390bf2fe13b7457c44
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2026-03-26 14:29:32 +0100

    ajout div container footer

--- assets/dist/main.bundle.js
+++ assets/dist/main.bundle.js
@@ -1393,7 +1393,11 @@ module.exports = styleTagTransform;
 >>>>>>> 048dcbb (npm run build)
 >>>>>>> 9d34631 (npm run build)
 const burgerBtn = document.querySelector('.burger-btn');
+<<<<<<< HEAD
 const mobileMenu = document.querySelector('#megamenu');
+=======
+const mobileMenu = document.querySelector('.mobile-menu');
+>>>>>>> abb23aa (ajout div container footer)
 
 if (burgerBtn && mobileMenu) {
     burgerBtn.addEventListener('click', () => {
@@ -1540,13 +1544,18 @@ const swiperGallery = new swiper__WEBPACK_IMPORTED_MODULE_0__["default"](".swipe
 =======
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 const swiperGallery = new swiper__WEBPACK_IMPORTED_MODULE_0__["default"](".swiper-gallery", {
   modules: [swiper_modules__WEBPACK_IMPORTED_MODULE_1__.Navigation, swiper_modules__WEBPACK_IMPORTED_MODULE_1__.Pagination, swiper_modules__WEBPACK_IMPORTED_MODULE_1__.Scrollbar],
 =======
 =======
 >>>>>>> 048dcbb (npm run build)
+=======
+>>>>>>> 3f11fe0 (ajout div container footer)
 
 
+=======
+>>>>>>> abb23aa (ajout div container footer)
 const swiperGallery = new swiper__WEBPACK_IMPORTED_MODULE_0__["default"](".swiper-gallery", {
   modules: [swiper_modules__WEBPACK_IMPORTED_MODULE_1__.Navigation, swiper_modules__WEBPACK_IMPORTED_MODULE_1__.Pagination],
 <<<<<<< HEAD
@@ -1585,6 +1594,7 @@ const swiperGallery = new swiper__WEBPACK_IMPORTED_MODULE_0__["default"](".swipe
 >>>>>>> 048dcbb (npm run build)
   },
 
+<<<<<<< HEAD
     pagination: {
         el: '.swiper-pagination-gallery',
         clickable: true,
@@ -1597,10 +1607,29 @@ const swiperGallery = new swiper__WEBPACK_IMPORTED_MODULE_0__["default"](".swipe
     loop: true,
     speed: 600,
 <<<<<<< HEAD
+<<<<<<< HEAD
 >>>>>>> 815fcfc (structure html)
 =======
 >>>>>>> 048dcbb (npm run build)
+<<<<<<< HEAD
 >>>>>>> 9d34631 (npm run build)
+=======
+=======
+=======
+  pagination: {
+    el: ".swiper-pagination-gallery",
+    clickable: true,
+    type: "bullets",
+  },
+
+  slidesPerView: 1.2,
+  //centeredSlides: 'auto',
+  spaceBetween: 200,
+  loop: true,
+  speed: 600,
+>>>>>>> abb23aa (ajout div container footer)
+>>>>>>> 3f11fe0 (ajout div container footer)
+>>>>>>> d41a9bc (ajout div container footer)
 });
 
 
