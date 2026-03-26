29c195607e3354f9e40dc2d257fc6e3b3cf93392
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2026-03-26 14:43:09 +0100

    npm run build

--- assets/dist/style.bundle.js
+++ assets/dist/style.bundle.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
@@ -9726,6 +9727,7 @@ __webpack_require__.r(__webpack_exports__);
 ;
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 //# sourceMappingURL=style.bundle.js.map
 =======
 >>>>>>> aec7400 (npm run build)
@@ -9737,3 +9739,8 @@ __webpack_require__.r(__webpack_exports__);
 =======
 //# sourceMappingURL=style.bundle.js.map
 >>>>>>> 5547920 (npm run dev)
+=======
+//# sourceMappingURL=style.bundle.js.map
+=======
+>>>>>>> fdaef99 (npm run build)
+>>>>>>> 1a60694 (npm run build)
