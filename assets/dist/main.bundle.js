f1c8f8db7c211856ce934989e480e32001fdb75f
caractere-advertising <anne@caractere-advertising.be>
2026-03-27 14:12:05 +0100

    structure html

--- assets/dist/main.bundle.js
+++ assets/dist/main.bundle.js
@@ -1494,6 +1494,7 @@ const swiperGallery = new swiper__WEBPACK_IMPORTED_MODULE_0__["default"](".swipe
 
 const swiperGallery = new swiper__WEBPACK_IMPORTED_MODULE_0__["default"](".swiper-gallery", {
 <<<<<<< HEAD
+<<<<<<< HEAD
   modules: [swiper_modules__WEBPACK_IMPORTED_MODULE_1__.Navigation, swiper_modules__WEBPACK_IMPORTED_MODULE_1__.Pagination],
 <<<<<<< HEAD
 >>>>>>> 815fcfc (structure html)
@@ -1502,6 +1503,9 @@ const swiperGallery = new swiper__WEBPACK_IMPORTED_MODULE_0__["default"](".swipe
 =======
   modules: [swiper_modules__WEBPACK_IMPORTED_MODULE_1__.Navigation, swiper_modules__WEBPACK_IMPORTED_MODULE_1__.Pagination, swiper_modules__WEBPACK_IMPORTED_MODULE_1__.Scrollbar],
 >>>>>>> 11459a4 (Optimisation section.php + css + ajout nouvelle section page "construire")
+=======
+  modules: [swiper_modules__WEBPACK_IMPORTED_MODULE_1__.Navigation, swiper_modules__WEBPACK_IMPORTED_MODULE_1__.Pagination],
+>>>>>>> 11a19a1 (structure html)
 
   navigation: {
     nextEl: ".gallery-next",
@@ -1511,6 +1515,7 @@ const swiperGallery = new swiper__WEBPACK_IMPORTED_MODULE_0__["default"](".swipe
   pagination: {
     el: ".swiper-pagination-gallery",
     clickable: true,
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -1531,6 +1536,10 @@ const swiperGallery = new swiper__WEBPACK_IMPORTED_MODULE_0__["default"](".swipe
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+=======
+  },
+
+>>>>>>> 11a19a1 (structure html)
     pagination: {
         el: '.swiper-pagination-gallery',
         clickable: true,
@@ -1542,6 +1551,7 @@ const swiperGallery = new swiper__WEBPACK_IMPORTED_MODULE_0__["default"](".swipe
     spaceBetween: 200,
     loop: true,
     speed: 600,
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 >>>>>>> 815fcfc (structure html)
@@ -1577,6 +1587,8 @@ const swiperGallery = new swiper__WEBPACK_IMPORTED_MODULE_0__["default"](".swipe
 >>>>>>> 657534c (npm run dev)
 =======
 >>>>>>> 11459a4 (Optimisation section.php + css + ajout nouvelle section page "construire")
+=======
+>>>>>>> 11a19a1 (structure html)
 });
 
 
