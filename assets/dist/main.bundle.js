6b21a334c547e54fa29009dad83a55ce76867a39
caractere-advertising <anne@caractere-advertising.be>
2026-03-27 14:12:05 +0100

    structure html

--- assets/dist/main.bundle.js
+++ assets/dist/main.bundle.js
@@ -24983,6 +24983,7 @@ links.forEach(link => {
 
 /***/ },
 
+<<<<<<< HEAD
 /***/ "./src/js/parallax.js"
 /*!****************************!*\
   !*** ./src/js/parallax.js ***!
@@ -25004,6 +25005,8 @@ new simple_parallax_js_vanilla__WEBPACK_IMPORTED_MODULE_0__["default"](image, {
 
 /***/ },
 
+=======
+>>>>>>> 815fcfc (structure html)
 /***/ "./src/js/scroll-top.js"
 /*!******************************!*\
   !*** ./src/js/scroll-top.js ***!
@@ -25037,8 +25040,15 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
+<<<<<<< HEAD
 const swiperGallery = new swiper__WEBPACK_IMPORTED_MODULE_0__["default"](".swiper-gallery", {
   modules: [swiper_modules__WEBPACK_IMPORTED_MODULE_1__.Navigation, swiper_modules__WEBPACK_IMPORTED_MODULE_1__.Pagination, swiper_modules__WEBPACK_IMPORTED_MODULE_1__.Scrollbar],
+=======
+
+
+const swiperGallery = new swiper__WEBPACK_IMPORTED_MODULE_0__["default"](".swiper-gallery", {
+  modules: [swiper_modules__WEBPACK_IMPORTED_MODULE_1__.Navigation, swiper_modules__WEBPACK_IMPORTED_MODULE_1__.Pagination],
+>>>>>>> 815fcfc (structure html)
 
   navigation: {
     nextEl: ".gallery-next",
@@ -25048,6 +25058,7 @@ const swiperGallery = new swiper__WEBPACK_IMPORTED_MODULE_0__["default"](".swipe
   pagination: {
     el: ".swiper-pagination-gallery",
     clickable: true,
+<<<<<<< HEAD
     type: "bullets",
   },
 
@@ -25058,6 +25069,21 @@ const swiperGallery = new swiper__WEBPACK_IMPORTED_MODULE_0__["default"](".swipe
   spaceBetween: 200,
   loop: true,
   speed: 600,
+=======
+  },
+
+    pagination: {
+        el: '.swiper-pagination-gallery',
+        clickable: true,
+        type:'bullets'
+    },
+
+    slidesPerView: 1.2,
+    //centeredSlides: 'auto',
+    spaceBetween: 200,
+    loop: true,
+    speed: 600,
+>>>>>>> 815fcfc (structure html)
 });
 
 
@@ -25169,6 +25195,7 @@ const swiperReference = new swiper__WEBPACK_IMPORTED_MODULE_0__["default"](".swi
   speed: 600,
 });
 
+<<<<<<< HEAD
 const swiperVertical = new swiper__WEBPACK_IMPORTED_MODULE_0__["default"](".swiper-vertical", {
   modules: [swiper_modules__WEBPACK_IMPORTED_MODULE_1__.Autoplay, swiper_modules__WEBPACK_IMPORTED_MODULE_1__.Pagination],
   autoplay: true,
@@ -25443,6 +25470,8 @@ class g {
 }
 
 
+=======
+>>>>>>> 815fcfc (structure html)
 
 /***/ },
 
@@ -36122,9 +36151,12 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _js_scroll_top_js__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(_js_scroll_top_js__WEBPACK_IMPORTED_MODULE_4__);
 /* harmony import */ var _js_img_hover_js__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./js/img-hover.js */ "./src/js/img-hover.js");
 /* harmony import */ var _js_img_hover_js__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(_js_img_hover_js__WEBPACK_IMPORTED_MODULE_5__);
+<<<<<<< HEAD
 /* harmony import */ var _js_parallax_js__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./js/parallax.js */ "./src/js/parallax.js");
 
 
+=======
+>>>>>>> 815fcfc (structure html)
 
 
 
