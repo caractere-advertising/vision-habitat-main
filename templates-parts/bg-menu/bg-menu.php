bb29b64e2733e95ac2354a23cb50c2638e86a0c1
caractere-advertising <anne@caractere-advertising.be>
2026-03-27 14:12:05 +0100

    structure html

--- templates-parts/bg-menu/bg-menu.php
+++ templates-parts/bg-menu/bg-menu.php
@@ -1,4 +1,12 @@
 <?php
+//champs page d'option acf
+//logo-social-media (répéteur -> image, lien)
+//menu-burger-footer(répéteur ->lien)
+//cond_générales (groupe ->cond(texte),copyright(texte))
+//bg-menu-image (image)
+//infos-contact(site-name(texte),street-name(texte),locality(texte), tel-num(texte),mail(texte),tva(texte))
+//logo_bg_menu(image)
+
 $burger_image        = get_field('bg-menu-image', 'option');
 $burger_info_contact = get_field('infos-contact', 'option');
 $burger_socials      = get_field('logo-social-media', 'option');
@@ -50,30 +58,15 @@ $logo_bg_menu        = get_field('logo_bg_menu', 'option');
     </div>
 
     <div class="menu-burger-right">
-<<<<<<< header
 
         <button class="burger-btn burger-btn-close">&#x2715;</button>
 
         <?php wp_nav_menu([
-=======
-        <button class="burger-btn
-         burger-btn-close">&#x2715;</button>
-
-        <?php
-        wp_nav_menu([
->>>>>>> 401cb8c (avancement bg-menu)
-=======
-
-        <button class="burger-btn burger-btn-close">&#x2715;</button>
-
-        <?php wp_nav_menu([
->>>>>>> d4eec9f (suite burger menu)
             'theme_location' => 'burger-menu',
             'container'      => 'nav',
             'container_class' => 'burger-nav',
             'menu_class'     => 'burger-menu_list',
-<<<<<<< HEAD
-<<<<<<< HEAD
+
         ]); ?>
 
         <div class="burger-bottom">
@@ -104,44 +97,3 @@ $logo_bg_menu        = get_field('logo_bg_menu', 'option');
     </div>
 
 </section>
-=======
-        ]);
-        ?>
-=======
-        ]); ?>
->>>>>>> d4eec9f (suite burger menu)
-
-        <?php if ($footer_nav) : ?>
-            <ul class="bg-menu">
-                <?php foreach ($footer_nav as $item) : ?>
-                    <li>
-                        <a href="<?= esc_url($item['link']['url']) ?>">
-                            <?= esc_html($item['link']['title']) ?>
-                        </a>
-                    </li>
-                <?php endforeach; ?>
-            </ul>
-        <?php endif; ?>
-
-        <?php if ($cond_generales) : ?>
-            <div class="burger-legal">
-                <hr>
-                <?php foreach ($cond_generales as $row) : ?>
-                    <p class="burger-legal_links"><?= esc_html($row['cond']) ?></p>
-                    <p class="burger-legal_copyright"><?= esc_html($row['copyright']) ?></p>
-                <?php endforeach; ?>
-            </div>
-        <?php endif; ?>
-
-    </div>
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-    </section>
->>>>>>> 401cb8c (avancement bg-menu)
-=======
-</section>
->>>>>>> d4eec9f (suite burger menu)
-=======
-</section>
->>>>>>> aa53866 (avancement bg-menu)
