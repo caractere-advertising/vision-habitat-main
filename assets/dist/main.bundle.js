38114cd36a112062464bee39531435a88d0fb4b9
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2026-03-30 10:43:24 +0200

    #

--- assets/dist/main.bundle.js
+++ assets/dist/main.bundle.js
@@ -23646,10 +23646,8 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
-
-
 const swiperGallery = new swiper__WEBPACK_IMPORTED_MODULE_0__["default"](".swiper-gallery", {
-  modules: [swiper_modules__WEBPACK_IMPORTED_MODULE_1__.Navigation, swiper_modules__WEBPACK_IMPORTED_MODULE_1__.Pagination],
+  modules: [swiper_modules__WEBPACK_IMPORTED_MODULE_1__.Navigation, swiper_modules__WEBPACK_IMPORTED_MODULE_1__.Pagination, swiper_modules__WEBPACK_IMPORTED_MODULE_1__.Scrollbar],
 
   navigation: {
     nextEl: ".gallery-next",
@@ -23659,19 +23657,16 @@ const swiperGallery = new swiper__WEBPACK_IMPORTED_MODULE_0__["default"](".swipe
   pagination: {
     el: ".swiper-pagination-gallery",
     clickable: true,
+    type: "bullets",
   },
 
-    pagination: {
-        el: '.swiper-pagination-gallery',
-        clickable: true,
-        type:'bullets'
-    },
-
-    slidesPerView: 1.2,
-    //centeredSlides: 'auto',
-    spaceBetween: 200,
-    loop: true,
-    speed: 600,
+  slidesPerView: 1.2,
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+  spaceBetween: 200,
+  loop: true,
+  speed: 600,
 });
 
 
