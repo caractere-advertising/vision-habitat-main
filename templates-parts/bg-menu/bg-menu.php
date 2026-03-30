553e53660aaa5726952dc0e40106e2103fc67b33
caractere-advertising <anne@caractere-advertising.be>
2026-03-30 13:14:17 +0200

    suite burger menu

--- templates-parts/bg-menu/bg-menu.php
+++ templates-parts/bg-menu/bg-menu.php
@@ -46,9 +46,11 @@ $logo_bg_menu        = get_field('logo_bg_menu', 'option');
             <?php endif; ?>
 
         </div>
+
     </div>
 
     <div class="menu-burger-right">
+<<<<<<< HEAD
 <<<<<<< HEAD
 
         <button class="burger-btn burger-btn-close">&#x2715;</button>
@@ -61,10 +63,17 @@ $logo_bg_menu        = get_field('logo_bg_menu', 'option');
         <?php
         wp_nav_menu([
 >>>>>>> 401cb8c (avancement bg-menu)
+=======
+
+        <button class="burger-btn burger-btn-close">&#x2715;</button>
+
+        <?php wp_nav_menu([
+>>>>>>> d4eec9f (suite burger menu)
             'theme_location' => 'burger-menu',
             'container'      => 'nav',
             'container_class' => 'burger-nav',
             'menu_class'     => 'burger-menu_list',
+<<<<<<< HEAD
 <<<<<<< HEAD
         ]); ?>
 
@@ -99,13 +108,17 @@ $logo_bg_menu        = get_field('logo_bg_menu', 'option');
 =======
         ]);
         ?>
+=======
+        ]); ?>
+>>>>>>> d4eec9f (suite burger menu)
 
         <?php if ($footer_nav) : ?>
-            <ul class="burger-cta-list">
+            <ul class="bg-menu">
                 <?php foreach ($footer_nav as $item) : ?>
                     <li>
-                        <a href="<?php echo $item['link']['url'];
-                                    ?>"><?php echo $item['link']['title']; ?></a>
+                        <a href="<?= esc_url($item['link']['url']) ?>">
+                            <?= esc_html($item['link']['title']) ?>
+                        </a>
                     </li>
                 <?php endforeach; ?>
             </ul>
@@ -115,14 +128,17 @@ $logo_bg_menu        = get_field('logo_bg_menu', 'option');
             <div class="burger-legal">
                 <hr>
                 <?php foreach ($cond_generales as $row) : ?>
-                    <p class="burger-legal_links"><?php echo
-                                                    $row['cond']; ?></p>
-                    <p class="burger-legal_copyright"><?php echo
-                                                        $row['copyright']; ?></p>
+                    <p class="burger-legal_links"><?= esc_html($row['cond']) ?></p>
+                    <p class="burger-legal_copyright"><?= esc_html($row['copyright']) ?></p>
                 <?php endforeach; ?>
             </div>
         <?php endif; ?>
+
     </div>
 
+<<<<<<< HEAD
     </section>
 >>>>>>> 401cb8c (avancement bg-menu)
+=======
+</section>
+>>>>>>> d4eec9f (suite burger menu)
