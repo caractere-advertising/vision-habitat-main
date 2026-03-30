5db4ff706f1e2a3ac245f11c8ed01952e2ddbfa8
caractere-advertising <anne@caractere-advertising.be>
2026-03-30 17:03:38 +0200

    fin 30-3

--- templates-parts/bg-menu/bg-menu.php
+++ templates-parts/bg-menu/bg-menu.php
@@ -1,13 +1,5 @@
 
 <?php
-//champs page d'option acf
-//logo-social-media (répéteur -> image, lien)
-//menu-burger-footer(répéteur ->lien)
-//cond_générales (groupe ->cond(texte),copyright(texte))
-//bg-menu-image (image)
-//infos-contact(site-name(texte),street-name(texte),locality(texte), tel-num(texte),mail(texte),tva(texte))
-//logo_bg_menu(image)
-
 $burger_image        = get_field('bg-menu-image', 'option');
 $burger_info_contact = get_field('infos-contact', 'option');
 $burger_socials      = get_field('logo-social-media', 'option');
@@ -32,9 +24,14 @@ $logo_bg_menu        = get_field('logo_bg_menu', 'option');
 <?php endif; ?>
 
         <div class="menu-burger-left-bottom">
+<<<<<<< HEAD
             <?php if ($burger_info_contact) :
                 $info = $burger_info_contact[0]; ?>
 
+=======
+            <?php if ($burger_info_contact) : ?>
+                <?php $info = $burger_info_contact[0]; ?>
+>>>>>>> 50d5368 (fin 30-3)
                 <div class="menu-burger-contact">
                     <p class="menu-burger-contact-name"><?php echo ($info['site-name']) ?></p>
                     <p><?php echo($info['street-name']) ?></p>
@@ -56,7 +53,6 @@ $logo_bg_menu        = get_field('logo_bg_menu', 'option');
                 
             <?php endif; ?>
         </div>
-
     </div>
     <div class="menu-burger-right">
         <button class="burger-btn burger-btn-close">&#x2715;</button>
@@ -85,7 +81,19 @@ $logo_bg_menu        = get_field('logo_bg_menu', 'option');
                 </ul>
             <?php endif; ?>
 
+<<<<<<< HEAD
            
+=======
+            <hr class="burger-hr">
+
+            <?php var_dump($cond_generales); ?>
+            <?php if ($cond_generales) : ?>
+                <div class="burger-legal-copy">
+                    <p class="burger-legal-links"><?= esc_html($cond_generales['cond']) ?></p>
+                    <p class="burger-legal-copyright"><?= esc_html($cond_generales['copyright']) ?></p>
+                </div>
+            <?php endif; ?>
+>>>>>>> 50d5368 (fin 30-3)
 
             <div class="burger-legal-copy">
                 <p class="burger-legal-links"><?php echo esc_html($cond_generales['cond']) ?></p>
