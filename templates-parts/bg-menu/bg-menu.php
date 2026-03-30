723125ccb2052b2317d8b7d1eceef46034e03a0d
caractere-advertising <anne@caractere-advertising.be>
2026-03-30 10:27:36 +0200

    avancement bg-menu

--- templates-parts/bg-menu/bg-menu.php
+++ templates-parts/bg-menu/bg-menu.php
@@ -63,7 +63,12 @@ $logo_bg_menu        = get_field('logo_bg_menu', 'option');
         <button class="burger-btn
          burger-btn-close">&#x2715;</button>
 
-        <?php wp_nav_menu([
+    <div class="menu-burger-right">
+        <button class="burger-btn
+         burger-btn-close">&#x2715;</button>
+
+        <?php
+        wp_nav_menu([
             'theme_location' => 'burger-menu',
             'container'      => 'nav',
             'container_class' => 'burger-nav',
