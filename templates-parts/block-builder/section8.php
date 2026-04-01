a86438bb73b8e7fbff9d44cc07a58b665f3af79a
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2026-04-01 12:28:31 +0200

    add if/else section 8

--- templates-parts/block-builder/section8.php
+++ templates-parts/block-builder/section8.php
@@ -12,19 +12,19 @@ $blueActif = get_sub_field('blue_activated');
 ?>
 
 <section class="section-citation">
+    <?php if($name):?>
+        <div class="citation-block container">
+            <div class="citation-inner container">
+                <?php if ($name) : ?>
+                <span class="citation-name"><?= $name; ?></span>
+                <?php endif; ?>
 
-    <div class="citation-block container">
-        <div class="citation-inner container">
-            <?php if ($name) : ?>
-            <span class="citation-name"><?= $name; ?></span>
-            <?php endif; ?>
-
-            <?php if($citation): ?>
-                <div class="citation-text"><?= $citation; ?></div>
-            <?php endif; ?>
-        </div>
+                <?php if($citation): ?>
+                    <div class="citation-text"><?= $citation; ?></div>
+                <?php endif; ?>
+            </div>
+    <?php endif;?>
     <?php if($blueActif):?>
-
         <div class="citation-blue">
             <div class="citation-blue-inner container">
                 <?php if ($title) : ?>
