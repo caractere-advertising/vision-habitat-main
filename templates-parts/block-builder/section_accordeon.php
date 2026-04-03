28f81d8f8d2c5ad6c7c84baea77dfee5bb31cb92
caractere-advertising <anne@caractere-advertising.be>
2026-04-03 14:54:42 +0200

    fin 3-04

---
+++ templates-parts/block-builder/section_accordeon.php
@@ -0,0 +1,33 @@
+<?php
+$small_title = get_sub_field('site_name');
+$image=get_sub_field('image');
+$title=get_sub_field('titre');
+$links=get_sub_field('lien_accordeon');
+?>
+
+<section>
+<div class="container">
+    <div class="col-image">
+        <?php if ($image) : ?>
+      <img src="<?= $image['url']; ?>" alt="<?= $image['alt']; ?>">
+        <?php endif; ?>
+    </div>
+
+    <div class="col-text">
+        <p class="small_title"><?php echo $small_title ?></p>
+   
+    <div class="title"><?php echo $title ?></div>
+    <ul class="accordeon">
+        <?php foreach ($links as $link): ?>
+        <li class="accordeon_el">
+            <a href="<?= esc_url($link['lien']['url']) ?>"><?= esc_html($link['lien']['title']) ?></a>
+</li>
+        </li>
+        <?php endforeach; ?>
+        
+       
+    </ul>
+ </div>
+</div>
+
+</section>
---
+++ templates-parts/block-builder/text_overlay.php
@@ -0,0 +1,25 @@
+<?php
+$title = get_sub_field('title');
+$image = get_sub_field('image');
+$paragraph = get_sub_field('paragraph');
+$link = get_sub_field('link');
+
+?>
+
+<section class="container">
+<div class="title">
+    <?php echo $title ?>
+</div>
+<div class="container_overlay">
+<div><img src="<?= $image['url']; ?>" alt="<?= $image['alt']; ?>"></div>
+<div>
+    <div class="paragraph"> <?php echo $paragraph ?></div>
+
+    <?php if($link):?>
+        <a class="btn-cta" href="<?= $link['url'];?>">
+            <?= $link['title'];?>
+        </a>     
+    <?php endif;?>
+</div>
+</div>
+</section>
