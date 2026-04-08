08604e2575cc57c5bf9698a65e3c41e001624c51
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2026-04-08 16:55:17 +0200

    Fix : Correction du burger-menu.scss + footer + functions + template-builder.php

--- footer.php
+++ footer.php
@@ -11,40 +11,29 @@ $made_by   = get_field('made_by','option');
     <div class="container">
         <div class="footer-top">
             <div class="footer-left">
-                <?php if ($text_ev) { ?>
-                    <div class="footer-titre"><?php echo $text_ev; ?></div>
-                <?php } ?>
-
+                <?php if($text_ev): ?><div class="footer-titre"><?= $text_ev; ?></div><?php endif; ?>
                 <svg xmlns="http://www.w3.org/2000/svg" class="footer-arrow" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" ><path d="m7 7 10 10"/><path d="M17 7v10H7"/></svg>        
             </div>
 
             <div class="footer-right">
-                <?php if ($paragraph) { ?>
-                    <div class="footer-texte"><?php echo $paragraph; ?></div>
-                <?php } ?>
+                <?php if($paragraph): ?><div class="footer-texte"><?= $paragraph; ?></div><?php endif; ?>
             </div>
         </div>
 
-        <?php if ($contact) { ?>
-            <a href="mailto:<?php echo $contact; ?>" class="footer-lien">
-                <?php echo $contact; ?>
-            </a>
-        <?php } ?>
+        <?php if ($contact): ?><a href="mailto:<?= $contact; ?>" class="footer-lien"><?= $contact; ?></a><?php endif; ?>
 
         <div class="footer-bottom">
-            <?php if ($made_by) { ?>
-                <div class="footer-made-by"><?php echo $made_by; ?></div>
-            <?php } ?>
-            <?php wp_nav_menu([
+            <?php if($made_by): ?><div class="footer-made-by"><?= $made_by; ?></div><?php endif;
+            
+            wp_nav_menu([
                 'theme_location' => 'footer',
                 'container'      => false,
                 'menu_class'     => 'footer-nav',
-            ]); ?>
+            ]);?>
         </div>
     </div>
 </footer>
 <a href="#" class="scroll-top">↖</a>
-
 
 <?php wp_footer(); ?>
 </body>
