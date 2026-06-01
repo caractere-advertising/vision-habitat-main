e2c033f3d4835aaca66d25fe6a64a6acf425ab79
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2026-06-01 22:56:55 +0200

    Correction section projets

--- templates-parts/block-builder/gallerie-partenaire-cont.php
+++ templates-parts/block-builder/gallerie-partenaire-cont.php
@@ -1,27 +1,28 @@
-
 <?php 
+
 $partenaires = get_field('gallerie-partenaires','option');
 $titre = get_field('titre','option');
+
 ?>
 
-
-
-
-    <div class="gallerie-partenaire swiper swiper-partenaire">
-        <div><?php echo $titre ?></div>
-      <div class="swiper-wrapper" >
-            <?php foreach ($partenaires as $partenaire) :
-                $image = $partenaire['image'];
-                $lien = $partenaire['lien']; ?>
-                <div class="partenaire swiper-slide">
-                    <a href="<?php echo $lien['url']; ?>">
-                            <?php if ($image) : ?>
-                            <img class="image"src="<?php echo $image['url']; ? loading="lazy">" alt="<?php echo $image['alt']; ?>">
-                            <?php endif; ?>
-                        </a>
-                </div>
-            <?php endforeach ?>
-            <div class="gallery-prev"></div>
-            <div class="gallery-next"> </div>
-        </div>
+<div class="gallerie-partenaire swiper swiper-partenaire">
+    <div><?php echo $titre ?></div>
+    
+    <div class="swiper-wrapper" >
+        <?php foreach ($partenaires as $partenaire) :
+            $image = $partenaire['image'];
+            $lien = $partenaire['lien']; ?>
+    
+            <div class="partenaire swiper-slide">
+                <a href="<?php echo $lien['url']; ?>">
+                    <?php if ($image) : ?>
+                        <img class="image"src="<?php echo $image['url']; ?>" loading="lazy" alt="<?php echo $image['alt']; ?>">
+                    <?php endif; ?>
+                </a>
+            </div>
+        <?php endforeach ?>
+    
+        <div class="gallery-prev"></div>
+        <div class="gallery-next"> </div>
     </div>
+</div>
