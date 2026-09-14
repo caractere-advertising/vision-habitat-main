3f1e6442ef9488cb595e2c02cb4499b9897e9ed6
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2026-09-14 16:42:00 +0200

    description

--- templates-parts/block-builder/section-galerie-references.php
+++ templates-parts/block-builder/section-galerie-references.php
@@ -17,7 +17,7 @@ if($type = 'references'){
             <?php foreach($slides as $s):
                 $image       = get_field('galerie', $s->ID)[0];
                 $titre       = $s->post_title;
-                $description = get_field('localite',$s->ID);
+                $description = get_field('description',$s->ID);
                 $link        = get_permalink($s->ID);
             ?>
 
