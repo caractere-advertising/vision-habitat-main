456cad8a88930957233c2009e43541d0ca20e049
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2026-09-14 17:31:48 +0200

    singl-eprojet

---
+++ single-projet.php
@@ -0,0 +1,91 @@
+<?php
+/* Template pour les pages "type" PROJET */
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+get_header();
+
+$date     = get_field( 'date' ) ?? '2026';
+$localite = get_field( 'localisation' ) ?? '';
+$archi    = get_field( 'architecte' ) ?? '';
+
+$galerie  = get_field( 'galerie' );
+
+if ( ! $galerie || ! is_array( $galerie ) ) {
+    get_footer();
+    return;
+}
+
+$img_url   = [];
+$img_title = [];
+
+$galerie2  = get_field('galerie-parallax');
+
+foreach ( $galerie2 as $i => $g ) {
+    $img_url[ $i ]   = $g['url'];
+    $img_title[ $i ] = $g['title'];
+}
+
+?>
+
+<!-- Bloc référence -->
+
+<section class="galerie-reference">
+    <div class="swiper swiper-reference">
+        <div class="swiper-wrapper">
+            <?php foreach ( $galerie as $g ) : ?>
+                <div class="swiper-slide">
+                    <img src="<?= esc_url( $g['url'] ); ?>" loading="lazy" alt="<?= esc_attr( $g['title'] ); ?>"/>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
+</section>
+
+<section class="bloc-reference">
+    <div class="container flex -full">
+        <div class="col-40 col-g">
+            <?php if ( ! empty( $img_url[0] ) ) : ?>
+                <div class="block-img fast-parallax">
+                    <img src="<?= esc_url( $img_url[0] ); ?>" loading="lazy" alt="<?= esc_attr( $img_title[0] ?? '' ); ?>">
+                </div>
+            <?php endif; ?>
+        </div>
+
+        <div class="col-d col-60">
+            <div class="section-informations">
+                <span class="date"><?= esc_html( $date ); ?></span>
+                <p><strong>Localité</strong><br><?= esc_html( $localite ); ?></p>
+                <p><strong>Architecte</strong><br><?= esc_html( $archi ); ?></p>
+            </div>
+            <?php if ( ! empty( $img_url[1] ) ) : ?>
+                <div class="block-img slow-parallax">
+                    <img src="<?= esc_url( $img_url[1] ); ?>" loading="lazy" alt="<?= esc_attr( $img_title[1] ?? '' ); ?>"/>
+                </div>
+            <?php endif; ?>
+        </div>
+    </div>
+    <div class="container flex -full -reverse">
+        <div class="col-60 col-g">
+            <?php if ( ! empty( $img_url[2] ) ) : ?>
+                <div class="block-img slow-parallax">
+                    <img src="<?= esc_url( $img_url[2] ); ?>" loading="lazy" alt="<?= esc_attr( $img_title[2] ?? '' ); ?>">
+                </div>
+            <?php endif; ?>
+        </div>
+
+        <div class="col-d col-40">
+            <?php if ( ! empty( $img_url[3] ) ) : ?>
+                <div class="block-img fast-parallax">
+                    <img src="<?= esc_url( $img_url[3] ); ?>" loading="lazy" alt="<?= esc_attr( $img_title[3] ?? '' ); ?>"/>
+                </div>
+            <?php endif; ?>
+        </div>
+    </div>
+</section>
+<?php
+
+get_template_part('templates-parts/builder');
+get_footer();
+
+?>
