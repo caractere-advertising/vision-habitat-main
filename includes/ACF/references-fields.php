7374bf581049c0aa5dd319fb6d9dc198deb33083
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2026-09-14 17:30:28 +0200

    acf field projet

--- includes/ACF/references-fields.php
+++ includes/ACF/references-fields.php
@@ -82,6 +82,11 @@ acf_add_local_field_group( array(
 				'operator' => '==',
 				'value' => 'reference',
 			),
+			array(
+				'param' => 'post_type',
+				'operator' => '==',
+				'value' => 'projet',
+			)
 		),
 	),
 
